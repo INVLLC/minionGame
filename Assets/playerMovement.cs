e84cb4de9836ac45e80b5eb6ccc285e67daec3db
Adrian Rosario <arosario@invisionnaire.com>
2022-10-23 21:25:46 -0500

    update 10/23/2022
    added a bg, fixed the jumping and falling. Still having issues with jumping still moving.

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
     private float dirX = 0;
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask jumpableGround;
     AudioSource jumpSound;
 
     // Start is called before the first frame update
@@ -19,6 +21,7 @@ public class playerMovement : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         jumpSound = GetComponent<AudioSource>();
+        coll = GetComponent<BoxCollider2D>();
     }//end of start
 
     // Update is called once per frame
@@ -29,11 +32,17 @@ public class playerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-         if(Input.GetButtonDown("Jump"))
+         if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(jumpableGround))
          {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpSound.Play();
-         } 
+
+            //change animation to jumping to true
+            anim.SetBool("isJumping", true);
+         } else {
+            //change animation to jumping to false
+            anim.SetBool("isJumping", false);
+         }
 
 
     UpdateAnimationState();
@@ -58,7 +67,18 @@ public class playerMovement : MonoBehaviour
         {
             anim.SetBool("isRunning", false);
         }
+
+    
+
+    
+
     }
+    private bool IsGrounded()
+        {
+            //check if player is touching ground
+            return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+
+        }
 
 }//end of class
   
